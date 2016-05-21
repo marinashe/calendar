3e83054f58fd082866d1337948fef80adfb301bd
Marina Sheenkova <msheenkova@gmail.com>
2016-05-22 00:18:20 +0300

    add href next/prev

--- server.py
+++ server.py
@@ -4,19 +4,38 @@ import datetime
 
 
 @route('/')
-def index():
-    cal_year = calendar.HTMLCalendar()
-    return cal_year.formatmonth(datetime.date.today().year, datetime.date.today().month)
-
 @route('/<year:int>/<month:int>/')
-def index(year, month):
+def index(year=None, month=None):
+    if not year:
+        year = int(datetime.date.today().year)
+        month = int(datetime.date.today().month)
     cal_year = calendar.HTMLCalendar()
-    return cal_year.formatmonth(year, month)
+
+    if month == 12:
+        pyear = year
+        pmonth = month - 1
+        nyear = year + 1
+        nmonth = 1
+    elif month == 1:
+        pyear = year - 1
+        pmonth = 12
+        nyear = year
+        nmonth = month + 1
+    else:
+        pyear = year
+        pmonth = month - 1
+        nyear = year
+        nmonth = month + 1
+    end = template('<br><a href="/{{pyear}}/{{pmonth}}/">Prev</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
+                   '<a href="/{{nyear}}/{{nmonth}}/">Next</a>', pyear=pyear, pmonth=pmonth, nyear=nyear, nmonth=nmonth)
+    return cal_year.formatmonth(year, month) + end
 
 @route('/<year:int>/')
 def index(year):
     cal_year = calendar.HTMLCalendar()
-    return cal_year.formatyear(year)
+    end = template('<br><a href="/{{pyear}}/">Prev</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
+                   '<a href="/{{nyear}}/">Next</a>', pyear=year-1, nyear=year+1)
+    return cal_year.formatyear(year) + end
 
 if __name__ == "__main__":
     run(debug=True, reloader=True)
