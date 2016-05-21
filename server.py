9b30b80c049416e1e82a31a2778856cd74cd0adf
Marina Sheenkova <msheenkova@gmail.com>
2016-05-21 23:29:04 +0300

    add months and year

--- server.py
+++ server.py
@@ -1,9 +1,22 @@
 from bottle import route, run, template
+import calendar
+import datetime
 
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+@route('/')
+def index():
+    cal_year = calendar.HTMLCalendar()
+    return cal_year.formatmonth(datetime.date.today().year, datetime.date.today().month)
+
+@route('/<year:int>/<month:int>/')
+def index(year, month):
+    cal_year = calendar.HTMLCalendar()
+    return cal_year.formatmonth(year, month)
+
+@route('/<year:int>/')
+def index(year):
+    cal_year = calendar.HTMLCalendar()
+    return cal_year.formatyear(year)
 
 if __name__ == "__main__":
     run(debug=True, reloader=True)
